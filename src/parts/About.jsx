5a3cb5c56676d81b7c23ab4e668deb1fcc1c8e7a
knikuji <143888272+knikuji@users.noreply.github.com>
2024-07-29 16:54:26 +0900

    URL変更

--- src/parts/About.jsx
+++ src/parts/About.jsx
@@ -4,7 +4,7 @@ const About = () => (
     <div>
         <h2>About Page</h2>
         <p>This is the about page.</p>
-        <a href="https://github.com/knikuji/React_Portfolio">Github of this portfolio</a>
+        <a href="https://github.com/knikuji/spa_test">Github of this portfolio</a>
     </div>
 );
 
