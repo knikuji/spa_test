16dc04f70e540243d2b15bb4b1dacea2f65ccbf7
knikuji <143888272+knikuji@users.noreply.github.com>
2024-07-31 16:58:58 +0900

    微修正

--- src/parts/About.jsx
+++ src/parts/About.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Qrcode from '../img/qr.png';
 
 const About = () => (
     <div className="grand_wrapper about_wrapper">
         <h2>About Page</h2>
         <a href="https://github.com/knikuji/spa_test">Github of this portfolio</a>
+        <h3>This site QRcode</h3>
+        <img src={Qrcode} alt="" className="qr_image" />
     </div>
 );
 
